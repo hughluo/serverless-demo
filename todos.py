377be962270ac1c0f0779b4e53fdda1ad8f6e00b
hughluo <bugwriter@outlook.com>
2020-06-05 09:39:47 +0200

    add createto test input

--- todos.py
+++ todos.py
@@ -1,6 +1,19 @@
 import json
 import os
+import boto3
+
 table_name = os.environ['TODO_TABLE']
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(table_name)
+
+def create(event, context):
+    body = {'event': event, 'context': context}
+    return {
+        "statusCode": 200,
+        "body": body,
+    }
+    
+
 def get_all(event, context):
     return {
         "statusCode": 200,
