9a47e6fbbdf6c6102bf060593f50350db35b0b7a
hughluo <bugwriter@outlook.com>
2020-06-05 23:11:57 +0200

    get_all pass, todo: create, get by id

--- serverless/todos.py
+++ serverless/todos.py
@@ -1,21 +1,66 @@
 import json
 import os
 import boto3
+import uuid
 
 table_name = os.environ['TODO_TABLE']
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(table_name)
 
 def create(event, context):
-    body = {'event': event, 'context': context}
+    event_json = json.loads(event)
+    if 'body' not in event_json or 'content' not in event_json.body:
+        return {
+            "statusCode": 400,
+            "body": f"must contain 'content' in request",
+        }
+    content = event_json.body.content
+    table.put_item(
+        Item={
+                'todoId': uuid.uuid1().hex,
+                'content': content,
+                'done': False,
+            }
+    )
     return {
         "statusCode": 200,
-        "body": body,
+        "body": 'success',
     }
     
 
 def get_all(event, context):
+    response = table.scan()
+    items = response['Items']
     return {
         "statusCode": 200,
-        "body": table_name,
+        "body": json.dumps(items),
+    }
+
+
+def get_by_id(event, context):
+    event_json = json.loads(event)
+    if 'pathParameters' not in event_json or 'param' not in event_json.pathParameters:
+        return {
+            "statusCode": 400,
+            "body": f"must contain 'content' in request",
+        }
+
+    todoId = event_json.pathParameters.param
+    response = table.get_item(
+        Key={
+            'todoId': todoId,
+        }
+    )
+
+    if 'Item' not in response:  # does not found the given id
+        return {
+            "statusCode": 404,
+            "body": f"todoId '{todoId}' not found",
+
+        }
+
+    item = response['Item']
+    return {
+        "statusCode": 200,
+        "body": json.dumps(item),
     }
